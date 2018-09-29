498918616495b6841ed066e3eba7735c2432dc5d
xudazhou <shin.12@163.com>
2018-09-29 17:20:28 +0800

    arraydemo

---
+++ arraydemo.cpp
@@ -0,0 +1,16 @@
+//
+// Created by xudazhou on 2018/9/29.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double arr1[3];
+    double arr2[3] = {1.1, 2.2, 3.3};
+
+    cout << arr2[0] << endl;
+    cout << *arr2 << endl;
+    cout << *(arr2 + 1) << endl;
+}
