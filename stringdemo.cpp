c04a312457f88e5b601c19575c0e453189fd84fa
xudazhou <shin.12@163.com>
2018-10-10 12:13:14 +0800

    string

--- stringdemo.cpp
+++ stringdemo.cpp
@@ -14,10 +14,11 @@ int main()
     cout << str1 << endl;
     cout << strlen(str1) << endl; // 5
     cout << sizeof(str1) << endl; // 6
-    return 0;
 
     string str2 = "world";
     cout << str2.length() << endl;
     cout << str2.size() << endl;
     cout << sizeof(str2) << endl;
+
+    return 0;
 }
